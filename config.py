d0d11349e10c045398aec23d2d94550950d5e80b
Snehashish Biswas <teamscenariox@gmail.com>
2022-05-22 21:04:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = ""
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "GitAlerts"
 
 # If you forked the source code, grab your repo's link and paste it below!
 GIT_REPO_URL = "https://github.com/TeamScenario/GitGram"
